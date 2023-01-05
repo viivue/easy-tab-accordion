0ee4d2140ea2193986b1b51957f9749ff372c188
vupham <vuquangpham2909@gmail.com>
2023-01-05 16:14:23 +0700

    :zap: getOptions > rename description

--- src/helpers.js
+++ src/helpers.js
@@ -240,9 +240,10 @@ export function getOptions(context, defaultOptions){
 
 
 /**
- * Get ID
+ * Is JSON string
+ * https://stackoverflow.com/a/32278428/6453822
  * @param string
- * @returns void
+ * @returns {any|boolean}
  */
 function isJSON(string){
     try{
