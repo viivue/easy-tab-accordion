eddde27dbc9f3374129a9b8217db57ebd2cb7f49
Pau15122 <77733880+vandangnhathung@users.noreply.github.com>
2023-08-01 17:44:29 +0700

    fix: format code

--- src/configs.js
+++ src/configs.js
@@ -2,18 +2,18 @@
  * Classes
  * */
 export const CLASSES = {
-  enabled: 'easy-tab-accordion-enabled',
-  active: 'active',
-  hasAssignedTriggerEvent: 'assigned-trigger-event',
+    enabled: 'easy-tab-accordion-enabled',
+    active: 'active',
+    hasAssignedTriggerEvent: 'assigned-trigger-event',
 };
 /**
  * Attributes
  * */
 export const ATTRS = {
-  container: 'data-eta',
-  trigger: 'data-eta-trigger',
-  receiver: 'data-eta-receiver',
-  hash: 'data-eta-hash',
-  hashScroll: 'data-eta-hash-scroll',
-  animation: 'data-eta-animation',
+    container: 'data-eta',
+    trigger: 'data-eta-trigger',
+    receiver: 'data-eta-receiver',
+    hash: 'data-eta-hash',
+    hashScroll: 'data-eta-hash-scroll',
+    animation: 'data-eta-animation',
 };
